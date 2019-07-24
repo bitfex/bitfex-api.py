74fd7c3adf557c6407fa9e3851fffa755146a803
BitFex <support@bitfex.trade>
2019-07-24 22:47:44 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
   name="bitfex",
-  version="1.0.0",
+  version="1.0.1",
   author="BitFex.Trade",
   author_email="support@bitfex.trade",
   description="BitFex.Trade python API wrapper",
